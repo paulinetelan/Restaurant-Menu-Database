37c7567ef777b9fcedfcbbcb4417e729233dd0fa
paulinetelan <pauline.telan@yahoo.com>
2016-03-30 11:52:37 -0600

    moved php files into webpage dir for now, http request works -- just not the GET req

---
+++ webpages/library.php
@@ -0,0 +1,36 @@
+/*
+	Actual library functions that get data
+	contains queries to database
+*/
+<?php
+	function getCustomers()
+	{
+		// Initialize array that will contain all data
+		$array = array();
+		
+		// Loop that populates array with data
+		for ($i = 0; $i < 5; $i++)
+		{
+			//Each userObjects = one user
+			$userObject = array(); // dictionary for one user
+			
+			$userObject["username"] = $i;
+			$userObject["fname"] = "User ".$i;
+			$userObject["lname"] = "LUser ".$i;
+			$userObject["password"] = "PUser ".$i;
+			$userObject["email"] = "EUser ".$i;
+			
+			// add user to array
+			$array[] = $userObject;
+		}
+		return $array;
+	}
+
+	// adds customer to database
+	function addCustomer($fname, $lname, $uname, $pw, $email)
+	{
+		return true;
+	}
+
+
+?>
