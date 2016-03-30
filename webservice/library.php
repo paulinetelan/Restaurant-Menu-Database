a3020271cdc73e613d271634d09d6adec41995cd
paulinetelan <pauline.telan@yahoo.com>
2016-03-29 22:28:03 -0600

    started webservice for addCustomer

---
+++ webservice/library.php
@@ -0,0 +1,35 @@
+/*
+	Actual library functions that get data
+*/
+<?php
+	function getCustomers()
+	{
+		// Initialize array that will contain all data
+		$array = array();
+		
+		// Loop that populates array with data
+		for ($i = 0; $i < 5; $i++)
+		{
+			//Each userObjects = one user
+			$userObject = array(); // dictionary for one user
+			
+			$userObject["username"] = $i;
+			$userObject["fname"] = "User ".$i;
+			$userObject["lname"] = "LUser ".$i;
+			$userObject["password"] = "PUser ".$i;
+			$userObject["email"] = "EUser ".$i;
+			
+			// add user to array
+			$array[] = $userObject;
+		}
+		return $array;
+	}
+
+	// adds customer to database
+	function addCustomer($fname, $lname, $username, $password, $email)
+	{
+		return true;
+	}
+
+
+?>
---
+++ webservice/webservice.php
@@ -0,0 +1,57 @@
+/**
+This php document returns data in JSON format
+**/
+
+<?php
+
+	// Include library that returns the data or execute the required methods
+	include("./library.php"); 
+
+	// CHECK REQUEST
+	// Check the method requested by site and execute
+	$method = $_GET["method"];
+	if($method == "getCustomers"){
+		   json_getCustomers();
+	}
+	else if($method == "addCustomer"){
+	     json_addCustomer($_GET["fname"], $_GET["lname"], $_GET["username"], $_GET["password"], $_GET["email"]);
+	}
+
+	// WEBSERVICE METHODS
+
+	function json_getCustomers(){
+		 $output = array();
+		 $output["success"] = true;
+		 $output["message"] = "Customer list fetched successfully";
+		 $output["data"] = getCustomers();
+		 echo json_encode($output);
+	}
+
+	function json_addCustomer($fname, $lname, $username, $password, $email){
+		 
+		 $valid = true;
+		 $message = "Customer added successfully";
+
+		 // Check if input values are taken
+		 if(!isset($fname) || !isset($lname) || !isset($username) || !isset($password) ||
+		 !isset($email) ){
+		 		$valid = false;
+				$message = "Invalid input values";
+		 }
+
+		 // Add user using library method
+		 if($valid)
+		 {
+			$valid = addCustomer($fname, $lname, $username, $password, $email);
+			if(!$valid){
+				$message = "Something went wrong with insertion process";
+			}
+		 }
+
+		 $output = array();
+		 $output["success"] = $valid;
+		 $output["message"] = $message;
+		 echo json_encode($output);
+	}
+
+?>
