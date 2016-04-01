38a852ef447b7a51edebbb9b971279a1c9ef18e1
paulinetelan <pauline.telan@yahoo.com>
2016-03-31 22:04:11 -0600

    created database (rest_database.sql) trying to connect webservice to db)

--- webpages/library.php
+++ webpages/library.php
@@ -4,32 +4,62 @@
 	Actual library functions that get data
 	contains queries to database
 */
+
+	$HOST = "localhost";
+	$C_USER = "root";
+	$C_PASSWORD = "1234";
+	$C_DATABASE = "rest_database";
+
+	$link = mysqli_connect($C_HOST, $C_USER, $C_PASSWORD, $C_DATABASE);
+	if (!$link)
+	{
+		echo mysqli_connect_error();
+	}
+
+	//////////// FUNCTIONS ///////////////////
+
 	function getCustomers()
 	{
 		// Initialize array that will contain all data
 		$array = array();
 		
-		// Loop that populates array with data
-		for ($i = 0; $i < 5; $i++)
+		//SQL statement
+		$sql = "SELECT DISTINCT fname, lname FROM Customer";
+
+		// execute sql statement
+		global $link;
+		$results = mysqli_query($link, $sql);
+
+		// retrieve rows returned
+		while ($row = mysqli_fetch_row($results))
 		{
-			//Each userObjects = one user
-			$userObject = array(); // dictionary for one user
-			
-			$userObject["username"] = $i;
-			$userObject["fname"] = "User ".$i;
-			$userObject["lname"] = "LUser ".$i;
-			$userObject["password"] = "PUser ".$i;
-			$userObject["email"] = "EUser ".$i;
-			
+			// each object represents one user
+			$object = array(); // used as a dictionary (fname, lname)
+
+			$object["fname"] = $row[0];
+			$object["lname"] = $row[1];
+
 			// add user to array
-			$array[] = $userObject;
+			$array[] = $object;
 		}
 		return $array;
 	}
 
-	// adds customer to database
+	// adds customer to database and returns true if successful
 	function addCustomer($fname, $lname, $uname, $pw, $email)
 	{
+		global $link;
+
+		// escape special characters to avoid sql injection
+		$name =  mysqli_real_escape_string($link, $fname, $lname, $uname, $pw, $email);
+
+		// create sql statement
+		$sql = "INSERT INTO Customer(uname, pw, fname, lname, email) VALUES ($uname, $pw, $fname, $lname, $email)";
+		echo $sql;
+
+		// execute sql statement
+		$results = mysqli_query($link, $sql);
+
 		return true;
 	}
 
